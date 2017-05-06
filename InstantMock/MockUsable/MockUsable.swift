1b7d7b83ca6cf1b297f4559bd88b2f605f484917
pirishd <pirishd@users.noreply.github.com>
2017-05-07 00:35:13 +0200

    Add MockUsable protocol

---
+++ InstantMock/MockUsable/MockUsable.swift
@@ -0,0 +1,19 @@
+//
+//  MockUsable.swift
+//  InstantMock
+//
+//  Created by Patrick on 06/05/2017.
+//  Copyright © 2017 pirishd. All rights reserved.
+//
+
+
+/** Protocol available for making a type easily mockable */
+public protocol MockUsable {
+
+    /// Represent any value for the type
+    var any: MockUsable { get }
+
+    /// Return true if self equals to provided value
+    func equal(to: MockUsable?) -> Bool
+
+}
