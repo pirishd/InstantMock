37a2b25193632e606ac33bd7741242f91525be6b
pirishd <pirishd@users.noreply.github.com>
2017-05-13 14:23:17 +0200

    Add unit tests for CallConfiguration

---
+++ InstantMockTests/Unit Tests/Interceptors/Helpers/CallConfigurationTests.swift
@@ -0,0 +1,37 @@
+//
+//  CallConfigurationTests.swift
+//  InstantMock
+//
+//  Created by Patrick on 13/05/2017.
+//  Copyright © 2017 pirishd. All rights reserved.
+//
+
+import XCTest
+@testable import InstantMock
+
+
+class CallConfigurationTests: XCTestCase {
+
+
+    func testEqual() {
+        let args1 = ArgumentsConfiguration([ArgumentValueMock(12)])
+        let args2 = ArgumentsConfiguration([ArgumentValueMock(13)])
+
+        let configuration1 = CallConfiguration(for: "func", with: args1)
+        let configuration2 = CallConfiguration(for: "func", with: args2)
+
+        XCTAssertEqual(configuration1, configuration2)
+    }
+
+
+    func testGreaterThan() {
+        let args1 = ArgumentsConfiguration([ArgumentValueMock(12), ArgumentValueMock(13)])
+        let args2 = ArgumentsConfiguration([ArgumentValueMock(13), ArgumentAnyMock()])
+
+        let configuration1 = CallConfiguration(for: "func", with: args1)
+        let configuration2 = CallConfiguration(for: "func", with: args2)
+
+        XCTAssertGreaterThan(configuration1, configuration2)
+    }
+
+}
