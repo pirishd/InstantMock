bc8562c00a501823aac6ac577960106c0783ff5f
pirishd <pirishd@users.noreply.github.com>
2017-05-20 13:28:25 +0200

    Make sure Arguments with verifications handle optional values

---
+++ InstantMockTests/Mocks/ArgumentVerifyOptionalMock.swift
@@ -0,0 +1,26 @@
+//
+//  ArgumentVerifyOptionalMock.swift
+//  InstantMock
+//
+//  Created by Patrick on 20/05/2017.
+//  Copyright © 2017 pirishd. All rights reserved.
+//
+
+import InstantMock
+
+
+class ArgumentVerifyOptionalMock<T>: ArgumentVerifyOptionalTyped {
+
+    var condition: (T?) -> Bool
+    required init(_ condition: @escaping ((T?) -> Bool)) {
+        self.condition = condition
+
+    }
+
+    var description: String { return "argument_verify_mock" }
+
+    func match(_ value: Any?) -> Bool {
+        return true
+    }
+
+}
