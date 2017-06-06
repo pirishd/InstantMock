f10171a3df5418039b961b7b3b02a5303c68f3ef
pirishd <pirishd@yahoo.com>
2017-06-06 22:44:58 +0200

    Add schemes for macOS

--- Sources/InstantMock.h
+++ Sources/InstantMock.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017 pirishd. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+@import Foundation;
 
 //! Project version number for InstantMock.
 FOUNDATION_EXPORT double InstantMockVersionNumber;
