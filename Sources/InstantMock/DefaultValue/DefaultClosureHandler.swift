bc55e0ffe647df320519b1c1f147aff23e0f5cea
pirishd <pirishd@icloud.com>
2019-05-05 18:55:36 +0200

    Simplify syntax

--- Sources/InstantMock/DefaultValue/DefaultClosureHandler.swift
+++ Sources/InstantMock/DefaultValue/DefaultClosureHandler.swift
@@ -12,37 +12,37 @@ class DefaultClosureHandler {
 
     /** Provide a default closure with provided signature (no args) */
     public static func it<Ret>() -> () -> Ret {
-        let closure: () -> Ret = { return Void() as! Ret }
+        let closure: () -> Ret = { return () as! Ret }
         return closure
     }
 
     /** Provide a default closure with provided signature (one arg) */
     public static func it<Arg1, Ret>() -> (Arg1) -> Ret {
-        let closure: (Arg1) -> Ret = { _ in return Void() as! Ret }
+        let closure: (Arg1) -> Ret = { _ in return () as! Ret }
         return closure
     }
 
     /** Provide a default closure with provided signature (2 args) */
     public static func it<Arg1, Arg2, Ret>() -> (Arg1, Arg2) -> Ret {
-        let closure: (Arg1, Arg2) -> Ret = { _, _ in return Void() as! Ret }
+        let closure: (Arg1, Arg2) -> Ret = { _, _ in return () as! Ret }
         return closure
     }
 
     /** Provide a default closure with provided signature (3 args) */
     public static func it<Arg1, Arg2, Arg3, Ret>() -> (Arg1, Arg2, Arg3) -> Ret {
-        let closure: (Arg1, Arg2, Arg3) -> Ret = { _, _, _ in return Void() as! Ret }
+        let closure: (Arg1, Arg2, Arg3) -> Ret = { _, _, _ in return () as! Ret }
         return closure
     }
 
     /** Provide a default closure with provided signature (4 args) */
     public static func it<Arg1, Arg2, Arg3, Arg4, Ret>() -> (Arg1, Arg2, Arg3, Arg4) -> Ret {
-        let closure: (Arg1, Arg2, Arg3, Arg4) -> Ret = { _, _, _, _ in return Void() as! Ret }
+        let closure: (Arg1, Arg2, Arg3, Arg4) -> Ret = { _, _, _, _ in return () as! Ret }
         return closure
     }
 
     /** Provide a default closure with provided signature (5 args) */
     public static func it<Arg1, Arg2, Arg3, Arg4, Arg5, Ret>() -> (Arg1, Arg2, Arg3, Arg4, Arg5) -> Ret {
-        let closure: (Arg1, Arg2, Arg3, Arg4, Arg5) -> Ret = { _, _, _, _, _ in return Void() as! Ret }
+        let closure: (Arg1, Arg2, Arg3, Arg4, Arg5) -> Ret = { _, _, _, _, _ in return () as! Ret }
         return closure
     }
 
