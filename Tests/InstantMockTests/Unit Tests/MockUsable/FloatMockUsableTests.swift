285c91342a505366b6b5b8fd0db46b3f0e51dd56
Arnaud Barisain-Monrose <abarisain@gmail.com>
2020-04-21 11:38:57 +0200

    numbers: add support for floats

---
+++ Tests/InstantMockTests/Unit Tests/MockUsable/FloatMockUsableTests.swift
@@ -0,0 +1,48 @@
+//
+//  FloatMockUsableTests.swift
+//  InstantMock
+//
+//  Created by Patrick on 06/05/2017.
+//  Copyright 2017 pirishd. All rights reserved.
+//
+
+import XCTest
+@testable import InstantMock
+
+
+final class FloatMockUsableTests: XCTestCase {
+
+
+    static var allTests = [
+        ("testEqual_toNil", testEqual_toNil),
+        ("testEqual_toWrongType", testEqual_toWrongType),
+        ("testEqual_toWrongValue", testEqual_toWrongValue),
+        ("testEqual_toExpectedValue", testEqual_toExpectedValue),
+    ]
+
+
+    func testEqual_toNil() {
+        let ret = (12.0 as Float).equal(to: nil)
+        XCTAssertFalse(ret)
+    }
+
+
+    func testEqual_toWrongType() {
+        let ret = (12.0 as Float).equal(to: 12)
+        XCTAssertFalse(ret)
+    }
+
+
+    func testEqual_toWrongValue() {
+        let ret = (12.0 as Float).equal(to: 13.0 as Float)
+        XCTAssertFalse(ret)
+    }
+
+
+    func testEqual_toExpectedValue() {
+        let ret = (12.0 as Float).equal(to: 12.0 as Float)
+        XCTAssertTrue(ret)
+    }
+
+
+}
