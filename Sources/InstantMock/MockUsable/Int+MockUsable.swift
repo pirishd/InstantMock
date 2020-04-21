54bb45ca38eae32a8bca312b2fa98d248b1abeca
Arnaud Barisain-Monrose <abarisain@gmail.com>
2020-04-21 12:20:41 +0200

    integers: return proper any values

--- Sources/InstantMock/MockUsable/Int+MockUsable.swift
+++ Sources/InstantMock/MockUsable/Int+MockUsable.swift
@@ -10,10 +10,6 @@
 /** Extension for making `Int` easily usage in mocks */
 extension FixedWidthInteger where Self: MockUsable {
 
-    public static var anyValue: MockUsable {
-        return Int.any
-    }
-
     public func equal(to value: MockUsable?) -> Bool {
         guard let intValue = value as? Self else { return false }
         return self == intValue
@@ -21,7 +17,31 @@ extension FixedWidthInteger where Self: MockUsable {
 
 }
 
-extension Int: MockUsable { public static let any = 42 }
-extension Int64: MockUsable { public static let any = 42 }
-extension UInt: MockUsable { public static let any = 42 }
-extension UInt64: MockUsable { public static let any = 42 }
+extension Int: MockUsable {
+    public static let any = 42
+    
+    public static var anyValue: MockUsable {
+        return Int.any
+    }
+}
+extension Int64: MockUsable {
+    public static let any = 42 as Int64
+    
+    public static var anyValue: MockUsable {
+        return Int64.any
+    }
+}
+extension UInt: MockUsable {
+    public static let any = 42 as UInt
+    
+    public static var anyValue: MockUsable {
+        return UInt.any
+    }
+}
+extension UInt64: MockUsable {
+    public static let any = 42 as UInt64
+    
+    public static var anyValue: MockUsable {
+        return UInt64.any
+    }
+}
