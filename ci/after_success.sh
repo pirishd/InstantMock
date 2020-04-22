723bf5b358d1abacc2a99c9b236434713142c189
pirishd <pirishd@icloud.com>
2020-04-22 18:17:38 +0200

    Probably missing a ";"

--- ci/after_success.sh
+++ ci/after_success.sh
@@ -2,5 +2,5 @@
 
 # run codecov on MacOS
 if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
-  bash <(curl -s https://codecov.io/bash) -J '^InstantMock$'
+  bash <(curl -s https://codecov.io/bash) -J '^InstantMock$';
 fi
