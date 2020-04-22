221c2373d82864f6bdc589d8e3b0ee576a7820a3
Arnaud Barisain-Monrose <abarisain@gmail.com>
2020-04-22 11:31:26 +0200

    date: add date MockUsable and tests

---
+++ Tests/InstantMockTests/Unit Tests/MockUsable/DateMockUsableTests.swift
@@ -0,0 +1,48 @@
+//
+//  StringMockUsableTests.swift
+//  InstantMock
+//
+//  Created by Arnaud Barisain-Monrose on 22/04/2020.
+//  Copyright 2017 pirishd. All rights reserved.
+//
+
+import XCTest
+@testable import InstantMock
+
+
+final class DateMockUsableTests: XCTestCase {
+
+
+    static var allTests = [
+        ("testEqual_toNil", testEqual_toNil),
+        ("testEqual_toWrongType", testEqual_toWrongType),
+        ("testEqual_toWrongValue", testEqual_toWrongValue),
+        ("testEqual_toExpectedValue", testEqual_toExpectedValue),
+    ]
+
+
+    func testEqual_toNil() {
+        let ret = Date(timeIntervalSince1970: 1234).equal(to: nil)
+        XCTAssertFalse(ret)
+    }
+
+
+    func testEqual_toWrongType() {
+        let ret = Date(timeIntervalSince1970: 1234).equal(to: 12)
+        XCTAssertFalse(ret)
+    }
+
+
+    func testEqual_toWrongValue() {
+        let ret = Date(timeIntervalSince1970: 1234).equal(to: Date(timeIntervalSince1970: 22222))
+        XCTAssertFalse(ret)
+    }
+
+
+    func testEqual_toExpectedValue() {
+        let ret = Date(timeIntervalSince1970: 1234).equal(to: Date(timeIntervalSince1970: 1234))
+        XCTAssertTrue(ret)
+    }
+
+
+}
