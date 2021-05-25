5a6980ff0a99d5bb7709a6de319ec2cc37533067
pirishd <pirishd@icloud.com>
2021-05-25 22:04:41 +0200

    Support capturing closures that can throw exceptions

--- Sources/InstantMock/Arguments/Captor/ArgumentClosureCaptor/ArgumentClosureCaptor+ThreeArgs.swift
+++ Sources/InstantMock/Arguments/Captor/ArgumentClosureCaptor/ArgumentClosureCaptor+ThreeArgs.swift
@@ -11,10 +11,18 @@
 extension ArgumentClosureCaptor {
 
 
-    /** Capture an argument of expected type */
+    /** Capture a closure of expected type */
     public func capture<Arg1, Arg2, Arg3, Ret>() -> T where T == (Arg1, Arg2, Arg3) -> Ret {
         let factory = ArgumentFactoryImpl<T>()
         return self.capture(argFactory: factory, argStorage: ArgumentStorageImpl.instance) as (Arg1, Arg2, Arg3) -> Ret
+    }
+
+
+    /** Capture a closure of expected type that can throw */
+    public func capture<Arg1, Arg2, Arg3, Ret>() -> T where T == (Arg1, Arg2, Arg3) throws -> Ret {
+        let factory = ArgumentFactoryImpl<T>()
+        return self.capture(argFactory: factory, argStorage: ArgumentStorageImpl.instance)
+            as (Arg1, Arg2, Arg3) throws -> Ret
     }
 
 
